18c0191977ae44899d3e50109c1821368b7da32f
Varun Thakore <vrnthakore@gmail.com>
2023-12-06 19:30:08 +0530

    Add test verify circuit

--- src/circuit.rs
+++ src/circuit.rs
@@ -38,3 +38,67 @@ where
 
     Ok(())
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::ops::Rem;
+    use pasta_curves::Fp;
+    use num_bigint::BigUint;
+    use rand::RngCore; 
+    use bellpepper_core::test_cs::TestConstraintSystem;
+    use bellpepper_ed25519::curve::Ed25519Curve;
+    use bellpepper_ed25519::circuit::AllocatedAffinePoint; 
+    use crate::ed25519::{sign, verify};
+
+    #[test]
+    fn test_verify_circuit() {
+        let q: BigUint = BigUint::parse_bytes(
+            b"1000000000000000000000000000000014def9dea2f79cd65812631a5cf5d3ed",
+            16
+        ).unwrap();
+        let g = Ed25519Curve::basepoint();
+
+        let mut h_bytes: [u8; 32] = [0; 32];
+        rand::thread_rng().fill_bytes(&mut h_bytes);
+        let h = BigUint::from_bytes_le(h_bytes.as_ref()).rem(q.clone());
+
+        let mut priv_key_bytes: [u8; 32] = [0; 32];
+        rand::thread_rng().fill_bytes(&mut priv_key_bytes);
+        let priv_key = BigUint::from_bytes_le(priv_key_bytes.as_ref()).rem(q.clone());
+
+        let pub_key = Ed25519Curve::scalar_multiplication(&g, &priv_key);
+
+        let (r, s) = sign(&h, &priv_key);
+        
+        let veri_sig = verify(&h, &pub_key, &r, &s);
+        assert!(veri_sig);
+
+        let mut cs = TestConstraintSystem::<Fp>::new();
+        let g_al = AllocatedAffinePoint::alloc_affine_point(&mut cs.namespace(|| "alloc base point"), &g).unwrap();
+        let pubkey_al = AllocatedAffinePoint::alloc_affine_point(&mut cs.namespace(|| "alloc pubkey"), &pub_key).unwrap();
+        let r_al = AllocatedAffinePoint::alloc_affine_point(&mut cs.namespace(|| "alloc r"), &r).unwrap();
+
+        let mut h_bits = h.to_radix_le(2);
+        h_bits.resize(253, 0u8);
+        let h_al: Vec<Boolean> = h_bits.into_iter().map(|i| Boolean::constant(i != 0)).collect();
+        assert_eq!(h_al.len(), 253);
+
+        let mut s_bits = s.to_radix_le(2);
+        s_bits.resize(253, 0u8);
+        let s_al: Vec<Boolean> = s_bits.into_iter().map(|i| Boolean::constant(i != 0)).collect();
+        assert_eq!(s_al.len(), 253);
+
+        let _ = verify_circuit(
+            &mut cs.namespace(|| "verify circuit"), 
+            g_al, 
+            pubkey_al, 
+            h_al, 
+            (r_al, s_al)
+        );
+
+        assert!(cs.is_satisfied());
+        assert_eq!(cs.num_constraints(), 1436175);
+        assert_eq!(cs.num_inputs(), 1);
+    }
+}
